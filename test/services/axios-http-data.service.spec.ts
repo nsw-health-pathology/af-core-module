af09015c214a511c81cec9a73efa4790f60b429f
Ryan Denton <ryan.denton@health.nsw.gov.au>
2021-08-22 15:05:12 +1000

    GH-13 Move hardcoded responses into variables for GET call tests.

--- test/services/axios-http-data.service.spec.ts
+++ test/services/axios-http-data.service.spec.ts
@@ -12,31 +12,37 @@ describe('AxiosHttpDataService', () => {
 
     it('should make an axios GET call', async () => {
 
+      const responseStatus = StatusCodes.OK;
+      const responseBody = { version: '1.0.0' };
+
       // Setup Mock Responses
       const mockAxios = new MockAdapter(Axios);
-      mockAxios.onGet('/version').reply(StatusCodes.OK, { version: '1.0.0' });
+      mockAxios.onGet('/version').reply(responseStatus, responseBody);
 
       const axiosHttp = new AxiosHttpDataService(Axios);
       const response = await axiosHttp.makeHttpGetCall('/version');
 
-      expect(response.status).to.be.equal(StatusCodes.OK);
-      expect(response.body).to.be.deep.equal({ version: '1.0.0' });
+      expect(response.status).to.be.equal(responseStatus);
+      expect(response.body).to.be.deep.equal(responseBody);
 
     });
 
     it('should return http error response on failure', async () => {
 
+      const responseStatus = StatusCodes.UNAUTHORIZED;
+      const responseBody = { message: 'Missing API Key' };
+
       // Setup Mock Responses
       const mockAxios = new MockAdapter(Axios);
-      mockAxios.onGet('/version').reply(StatusCodes.UNAUTHORIZED, { message: 'Missing API Key' });
+      mockAxios.onGet('/version').reply(responseStatus, responseBody);
 
       const axiosHttp = new AxiosHttpDataService(Axios);
       const response = await axiosHttp.makeHttpGetCall('/version');
 
-      expect(response.status).to.be.equal(StatusCodes.UNAUTHORIZED);
-      expect(response.body).to.be.deep.equal({ message: 'Missing API Key' });
+      expect(response.status).to.be.equal(responseStatus);
+      expect(response.body).to.be.deep.equal(responseBody);
       expect(response.error).to.be.deep.equal({
-        data: { message: 'Missing API Key' },
+        data: responseBody,
         message: 'Request failed with status code 401',
         name: 'Error'
       });
@@ -45,6 +51,8 @@ describe('AxiosHttpDataService', () => {
 
     it('should return internal server error on critical failure', async () => {
 
+      const responseStatus = StatusCodes.INTERNAL_SERVER_ERROR;
+
       // Setup Mock Responses
       const mockAxios = new MockAdapter(Axios);
       mockAxios.onGet('/version').networkError();
@@ -52,7 +60,7 @@ describe('AxiosHttpDataService', () => {
       const axiosHttp = new AxiosHttpDataService(Axios);
       const response = await axiosHttp.makeHttpGetCall('/version');
 
-      expect(response.status).to.be.equal(StatusCodes.INTERNAL_SERVER_ERROR);
+      expect(response.status).to.be.equal(responseStatus);
       expect(response.body).to.be.deep.equal({});
       expect(response.error).to.be.deep.equal({
         data: 'API Call Failed. Network Error',
